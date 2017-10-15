74eea2dd7f1de6d3aaa398da82da80f386edb15f
demon song <787423495@qq.com>
2017-10-15 19:26:05 +0800

    698.Partition to K Equal Sum Subsets

--- src/com/daimens/algorithm/october/SolutionDay15_L0698.java
+++ src/com/daimens/algorithm/october/SolutionDay15_L0698.java
@@ -2,7 +2,7 @@ package com.daimens.algorithm.october;
 
 import java.util.Arrays;
 
-public class SolutionDay15_L0502 {
+public class SolutionDay15_L0698 {
 	
     public boolean canPartitionKSubsets(int[] nums, int k) {
         int sum = 0;
@@ -38,8 +38,42 @@ public class SolutionDay15_L0502 {
     	return false;
     }
     
+//    public boolean canPartitionKSubsets(int[] nums, int k) {
+//        int sum = 0;
+//        int max = 0;
+//        int n = nums.length;
+//        for (int i = 0; i < n; ++i) {
+//        	sum += nums[i];
+//        	max = Math.max(max, nums[i]);
+//        }
+//        if (sum % k != 0) return false;
+//        tar = sum / k;
+//        if (max > tar) return false;
+//        Arrays.sort(nums);
+//        return go(nums, 0, k, new int[k]);
+//    }
+//    
+//    int tar = 0;
+//    boolean go(int[] nums, int pos, int k, int[] sums) {
+//    	if (pos == nums.length) {
+//    		boolean check = true;
+//	    	for (int i = 0; i < k; ++i) {
+//	    		if (sums[i] != tar) check = false;
+//	    	}
+//	    	return check;
+//    	}
+//    	for (int i = 0; i < k; ++i) {
+//    		sums[i] += nums[pos];
+//    		if (sums[i] <= tar) {
+//    			if (go(nums, pos + 1, k, sums)) return true;
+//    		}
+//    		sums[i] -= nums[pos];
+//    	}
+//    	return false;
+//    }
+    
 	public static void main(String[] args) {
-		SolutionDay15_L0502 day = new SolutionDay15_L0502();
+		SolutionDay15_L0698 day = new SolutionDay15_L0698();
 		int[] nums = {4, 3, 2, 3, 5, 2, 1};
 		System.out.println(day.canPartitionKSubsets(nums, 4));
 	}
