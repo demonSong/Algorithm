0dec601c87e1a0726326b3ab51c541cf2b4070c5
demon song <787423495@qq.com>
2017-11-19 18:30:19 +0800

    731.My Calendar II

--- src/com/daimens/algorithm/november/MyCalendarTwo.java
+++ src/com/daimens/algorithm/november/MyCalendarTwo.java
@@ -2,73 +2,100 @@ package com.daimens.algorithm.november;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MyCalendarTwo {
 	
-	class Interval implements Comparable<Interval>{
-		int s;
-		int e;
-		Interval(int s, int e){
-			this.s = s;
-			this.e = e;
-		}
-		
-		@Override
-		public int compareTo(Interval o) {
-			return this.s == o.s ? this.e - o.e : this.s - o.s;
-		}
-		
-		@Override
-		public String toString() {
-			return "[" + s + ", " + e +"]";
-		}
-	}
 	
-	List<Interval> mem;
+	Map<Integer, Integer> map;
 	
 	public MyCalendarTwo() {
-        mem = new ArrayList<>();
+		map = new HashMap<>();
     }
     
     public boolean book(int start, int end) {
-    	Interval candicate = new Interval(start, end);
-        if (tripleOverlap(candicate)) {
-        	return false;
+        map.put(start, map.getOrDefault(start, 0) + 1);
+        map.put(end, map.getOrDefault(end, 0) - 1);
+        
+        int count = 0;
+        List<Integer> keys = new ArrayList<>(map.keySet());
+        Collections.sort(keys);
+        for (int key : keys) {
+        	count += map.get(key);
+        	if (count >= 3) {
+        		 map.put(start, map.getOrDefault(start, 0) - 1);
+        	     map.put(end, map.getOrDefault(end, 0) + 1);
+        	     return false;
+        	}
         }
-        else {
-        	mem.add(candicate);
-        	return true;
-        }
+        return true;
     }
-    
-    // 在 它的范围内不能出现 overlap
-    boolean tripleOverlap(Interval candicate) {
-    	Collections.sort(mem);
-        int n = mem.size();
-        int left = 0;
-        int right = n - 1;
-        while (left < n && candicate.s >= mem.get(left).e) left ++;
-        while (right >= 0 && candicate.e <= mem.get(right).s) right --;
-        for (int i = left; i <= right; ++i) {
-            for (int j = left; j < i; ++j){
-                Interval ans = new Interval(-1, -1);
-                if (overlap(mem.get(j), mem.get(i), ans)) {
-                    if (overlap(ans, candicate, new Interval(-1, -1))) return true;
-                }
-            }
-    	}
-    	return false;
-    }
-    
-    boolean overlap(Interval a, Interval b, Interval ans) {
-    	if (b.s >= a.s && b.s < a.e || b.e <= a.e && b.e > a.s || b.s <= a.s && b.e >= a.e) {
-    		ans.s = Math.max(b.s, a.s);
-    		ans.e = Math.min(a.e, b.e);
-    		return true;
-    	}
-    	return false;
-    }
+	
+//	class Interval implements Comparable<Interval>{
+//		int s;
+//		int e;
+//		Interval(int s, int e){
+//			this.s = s;
+//			this.e = e;
+//		}
+//		
+//		@Override
+//		public int compareTo(Interval o) {
+//			return this.s == o.s ? this.e - o.e : this.s - o.s;
+//		}
+//		
+//		@Override
+//		public String toString() {
+//			return "[" + s + ", " + e +"]";
+//		}
+//	}
+//	
+//	List<Interval> mem;
+//	
+//	public MyCalendarTwo() {
+//        mem = new ArrayList<>();
+//    }
+//    
+//    public boolean book(int start, int end) {
+//    	Interval candicate = new Interval(start, end);
+//        if (tripleOverlap(candicate)) {
+//        	return false;
+//        }
+//        else {
+//        	mem.add(candicate);
+//        	return true;
+//        }
+//    }
+//    
+//    // 在 它的范围内不能出现 overlap
+//    boolean tripleOverlap(Interval candicate) {
+//    	Collections.sort(mem);
+//        int n = mem.size();
+//        int left = 0;
+//        int right = n - 1;
+//        while (left < n && candicate.s >= mem.get(left).e) left ++;
+//        while (right >= 0 && candicate.e <= mem.get(right).s) right --;
+//        for (int i = left; i <= right; ++i) {
+//            for (int j = left; j < i; ++j){
+//                Interval ans = new Interval(-1, -1);
+//                if (overlap(mem.get(j), mem.get(i), ans)) {
+//                    if (overlap(ans, candicate, new Interval(-1, -1))) return true;
+//                }
+//            }
+//    	}
+//    	return false;
+//    }
+//    
+//    boolean overlap(Interval a, Interval b, Interval ans) {
+//    	if (b.s >= a.s && b.s < a.e || b.e <= a.e && b.e > a.s || b.s <= a.s && b.e >= a.e) {
+//    		ans.s = Math.max(b.s, a.s);
+//    		ans.e = Math.min(a.e, b.e);
+//    		return true;
+//    	}
+//    	return false;
+//    }
     
     public static void main(String[] args) {
     	MyCalendarTwo MyCalendar = new MyCalendarTwo();
