c38aec6e01d86dfd660bba45936616413a2bc3fc
demon song <787423495@qq.com>
2017-11-19 17:36:17 +0800

    729.My calendar I

--- src/com/daimens/algorithm/november/SolutionDay19_L0501.java
+++
@@ -1,9 +0,0 @@
-package com.daimens.algorithm.november;
-
-public class SolutionDay19_L0501 {
-	
-	
-	public static void main(String[] args) {
-		SolutionDay19_L0501 day = new SolutionDay19_L0501();
-	}
-}
